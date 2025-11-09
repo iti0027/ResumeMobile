785c479bb4b16920abc05d7dc3f2f3525f7eca1b
Ítalo <163360684+iti0027@users.noreply.github.com>
2025-11-09 00:30:34 +0000

    configurando o tabBar com icones

--- resumemobile/app/constants/colors.tsx
+++ resumemobile/app/constants/colors.tsx
@@ -4,7 +4,7 @@ export const Colors = {
     background: '#273025ff',
     primaryText: '#ffffffc4',
     secondaryText: '#3c942fa8',
-    backgroundBolder: '#1F331B'
+    backgroundBolder: '#2b5f21ff'
 
 
 }
--- resumemobile/app/pages/home/homeTabs.tsx
+++ resumemobile/app/pages/home/homeTabs.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Colors } from "../../constants/colors";
+import {Ionicons} from '@expo/vector-icons';
 
 import AboutMe from "../aboutMe/index";
 import Academics from "../academics/index";
@@ -11,7 +13,54 @@ const Tab = createBottomTabNavigator();
 
 export default function HomeTabs() {
     return(
-        <Tab.Navigator>
+        <Tab.Navigator
+                  screenOptions= {({ route }) => ({
+                    tabBarIcon: ({color, size}) => {
+                        let iconName;
+
+                        switch (route.name) {
+                            case 'About Me':
+                                iconName = 'person-circle-outline';
+                                break;
+                            case 'Academics':
+                                iconName = 'school-outline';
+                                break;
+                            case 'Professional':
+                                iconName = 'briefcase-outline';
+                                break;
+                            case 'Projects':
+                                iconName = 'code-slash-outline';
+                                break;
+                            default:
+                                iconName = 'ellipse-outline';
+                        }
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                    tabBarActiveTintColor: Colors.primaryText,
+                    tabBarInactiveTintColor: Colors.secondaryText,
+                    tabBarStyle: {
+                        backgroundColor: Colors.backgroundBolder,
+                        borderTopColor: Colors.backgroundBolder,
+                        borderTopWidth: 0,
+                        height: 60,
+                    },
+                    tabBarLabelStyle: {
+                        fontSize: 12,
+                        fontWeight: '600',
+                    },
+                    headerStyle:{
+                        backgroundColor: Colors.backgroundBolder,
+                        elevation: 0,
+                        shadowOpacity: 0,
+                        borderBottomWidth: 0,
+                    },
+                    headerTitleStyle: {
+                        fontSize: 20,
+                        fontWeight: 'bold',
+                        color: Colors.primaryText,
+                    },
+                  })}
+        >
             <Tab.Screen name="About Me" component={AboutMe} />
             <Tab.Screen name="Academics" component={Academics} />
             <Tab.Screen name="Professional" component={Professional} />
